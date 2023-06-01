af42198cde9888792b5cfe8ba937ab0ec8a0fda5
Anthony Umemoto <atumemot@ucsc.edu>
2023-06-01 10:28:38 -0700

    progress

--- game.js
+++ game.js
@@ -4,8 +4,8 @@ class Gameplay extends Phaser.Scene {
 	}
 
 	preload() {
-		this.load.image("rolypoly", "./rp.png");
-		this.load.image("slug", './slug.png');
+		this.load.image("rolypoly", "./assets/rp.png");
+		this.load.image("slug", './assets/slug.png');
 	}
 
 	create() {
@@ -15,18 +15,21 @@ class Gameplay extends Phaser.Scene {
 
 		this.rlyply = this.physics.add.sprite(100, 600, "rolypoly")
 			.setScale(0.7)
-			.setMaxVelocity(200, 300)
+			.setMaxVelocity(200, 400)
 			.setVelocityX(-this.MAX_VELOCITY)
 			.setSize(310, 200);
-
 		this.rlyply.flipX = true;
 
-		this.slug = this.physics.add.sprite(1400, 880, "slug");
-		this.slug.setScale(0.7);
-		this.slug.setMaxVelocity(this.SLUG_VELOCITY, 0);
-		this.slug.setSize(200, 100);
-		this.slug.setVelocityX(-this.SLUG_VELOCITY);
-		this.slug.setImmovable(true);
+		this.slug = this.physics.add.sprite(1400, 880, "slug")
+			.setScale(0.7)
+			.setMaxVelocity(this.SLUG_VELOCITY, 0)
+			.setSize(200, 100)
+			.setVelocityX(-this.SLUG_VELOCITY)
+			.setImmovable(true);
+
+		this.text = this.add.text(700, 200, "click or tap to jump")
+			.setFontSize("40pt")
+			.setOrigin(0.5, 0.5);
 
 		//ground rectangle
 		this.ground = new Phaser.GameObjects.Rectangle(this, 700, 1000, 1400, 150, 0x0000ff);
@@ -40,18 +43,21 @@ class Gameplay extends Phaser.Scene {
 		this.physics.add.collider(this.slug, this.ground);
 
 		this.spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-
+		// add a pointer down event listener
+		this.input.on('pointerdown', () => {
+			if (!this.rlyply.body.touching.down) return;
+			let vel = this.rlyply.body.velocity.y;
+			vel -= 400;
+			this.rlyply.setVelocityY(vel);
+		});
 	}
 
 	update(time, delta) {
 		if (this.rlyply.body.touching.down && Phaser.Input.Keyboard.JustDown(this.spaceKey)) {
 			let vel = this.rlyply.body.velocity.y;
-			vel -= 300;
-			//console.log(vel);
+			vel -= 400;
 			this.rlyply.setVelocityY(vel);
 		}
-
-
 
 		if (this.rlyply.x > 1400) {
 			this.displayTextAndReset("You Win");
@@ -61,25 +67,106 @@ class Gameplay extends Phaser.Scene {
 		}
 	}
 
-	handleCollision(player, object) {
-		console.log("we in");
-		if (player.body.touching.down) {
-			player.body.setVelocityX(-100);
-		} else {
-			player.body.setVelocityX(100);
+	handleCollision(player) {
+		if (player.body.touching.down && this.rlyply.y > 854 && this.rlyply.x < 856) {
+			this.scene.start("Gameplay2");
+		}
+		// } else {
+		// 	player.body.setVelocityX(-125);
+		// }
+	}
 
+	displayTextAndReset(text, reset = true) {
+		this.text.setText(text);
+		this.time.delayedCall(3000, () => {
+			if (reset) this.scene.restart();
+		});
+	}
+}
+
+class Gameplay2 extends Phaser.Scene {
+	constructor() {
+		super("Gameplay2");
+	}
+
+	preload() {
+		this.load.image("rolypoly", "./assets/rp.png");
+		this.load.image("slug", './assets/slug.png');
+	}
+
+	create() {
+		this.MAX_VELOCITY = -100;
+		this.SLUG_VELOCITY = 100;
+		this.restarting = false;
+
+		this.rlyply = this.physics.add.sprite(100, 600, "rolypoly")
+			.setScale(0.7)
+			.setMaxVelocity(200, 400)
+			.setVelocityX(-this.MAX_VELOCITY)
+			.setSize(310, 200);
+		this.rlyply.flipX = true;
+
+		this.slug = this.physics.add.sprite(1400, 880, "slug")
+			.setScale(0.7)
+			.setMaxVelocity(this.SLUG_VELOCITY, 0)
+			.setSize(200, 100)
+			.setVelocityX(-this.SLUG_VELOCITY)
+			.setImmovable(true);
+
+		this.text = this.add.text(700, 200, "click or tap to jump")
+			.setFontSize("40pt")
+			.setOrigin(0.5, 0.5);
+
+		//ground rectangle
+		this.ground = new Phaser.GameObjects.Rectangle(this, 700, 1000, 1400, 150, 0x0000ff);
+		this.physics.add.existing(this.ground);
+		this.ground.body.immovable = true;
+		this.ground.body.allowGravity = false;
+
+		//collision
+		this.physics.add.overlap(this.rlyply, this.slug, this.handleCollision, null, this);
+		this.physics.add.collider(this.rlyply, this.ground);
+		this.physics.add.collider(this.slug, this.ground);
+
+		this.spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+		// add a pointer down event listener
+		this.input.on('pointerdown', () => {
+			if (!this.rlyply.body.touching.down) return;
+			let vel = this.rlyply.body.velocity.y;
+			vel -= 400;
+			this.rlyply.setVelocityY(vel);
+		});
+	}
+
+	update(time, delta) {
+		if (this.rlyply.body.touching.down && Phaser.Input.Keyboard.JustDown(this.spaceKey)) {
+			let vel = this.rlyply.body.velocity.y;
+			vel -= 400;
+			this.rlyply.setVelocityY(vel);
+		}
+
+		if (this.rlyply.x > 1400) {
+			this.displayTextAndReset("You Win");
+		}
+		if (this.rlyply.x < -30) {
+			this.displayTextAndReset("You Lose");
 		}
 	}
 
-	displayTextAndReset(text) {
-
-		if (this.restarting != true) {
-			this.restarting = true;
-			this.add.text(550, 200, text).setFontSize("40pt");
-			this.time.delayedCall(3000, () => {
-				this.scene.restart();
-			});
+	handleCollision(player) {
+		if (player.body.touching.down && this.rlyply.y > 854 && this.rlyply.x < 856) {
+			this.scene.start("Gameplay");
 		}
+		// } else {
+		// 	player.body.setVelocityX(-125);
+		// }
+	}
+
+	displayTextAndReset(text, reset = true) {
+		this.text.setText(text);
+		this.time.delayedCall(3000, () => {
+			if (reset) this.scene.restart();
+		});
 	}
 }
 
